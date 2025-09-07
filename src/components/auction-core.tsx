4ae41e53165b825e66a833464ee0b05180c2c8e5
Venkata Pathiraju Padidapu <venkatapathirajupadidapu@gmail.com>
2025-09-07 18:55:23 +0000

    switched the positions player details and biding controls in the auction

--- src/components/auction-core.tsx
+++ src/components/auction-core.tsx
@@ -5,10 +5,10 @@ export default function AuctionCore() {
   return (
     <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 h-full">
       <div className="h-full">
-        <AuctionControls />
+        <PlayerDisplay />
       </div>
       <div className="h-full">
-        <PlayerDisplay />
+        <AuctionControls />
       </div>
     </div>
   );
